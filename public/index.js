1665e604e7c6fdde324f353b88c7194f3433ee8f
John D. Martin III <me@johndmart.in>
2022-04-07 01:44:44 -0400

    Create index.js

---
+++ public/index.js
@@ -0,0 +1,8 @@
+// Focus div based on nav button click
+
+// Flip one coin and show coin image to match result when button clicked
+
+// Flip multiple coins and show coin images in table as well as summary results
+// Enter number and press button to activate coin flip series
+
+// Guess a flip by clicking either heads or tails button
